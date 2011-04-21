3f1b8e0d058d760ae6dc8cb7d634baa72b6739cb
Matt Rubin <mattrubin@me.com>
2011-04-21 15:20:58 -0700

    Partial implementation of MMU (doesn't compile cleanly)

--- MMU.c
+++ MMU.c
@@ -7,3 +7,96 @@
 //
 
 #include "MMU.h"
+
+
+void initMMU(MMU* mmu){
+	mmu->inBIOS = true;
+	// write bios to mmu->bios
+}
+
+byte readByte(MMU* mmu, addr address){
+	switch(address & 0xF000){
+		// BIOS (256b)/ROM0
+		case 0x0000:
+			if(mmu->inBIOS)
+			{
+				if(address < 0x0100)
+					return mmu->bios[address];
+				else if(Z80._r.pc == 0x0100)
+					mmu->inBIOS = false;
+			}
+			
+			return mmu->rom[address];
+			
+		// ROM0
+		case 0x1000:
+		case 0x2000:
+		case 0x3000:
+			return mmu->rom[address];
+			
+		// ROM1 (unbanked) (16k)
+		case 0x4000:
+		case 0x5000:
+		case 0x6000:
+		case 0x7000:
+			return mmu->rom[address];
+			
+		// Graphics: VRAM (8k)
+		case 0x8000:
+		case 0x9000:
+			return GPU->vram[address & 0x1FFF];
+			
+		// External RAM (8k)
+		case 0xA000:
+		case 0xB000:
+			return mmu->eram[address & 0x1FFF];
+			
+		// Working RAM (8k)
+		case 0xC000:
+		case 0xD000:
+			return mmu->wram[address & 0x1FFF];
+			
+		// Working RAM shadow
+		case 0xE000:
+			return mmu->wram[address & 0x1FFF];
+			
+		// Working RAM shadow, I/O, Zero-page RAM
+		case 0xF000:
+			switch(address & 0x0F00){
+				// Working RAM shadow
+				case 0x000: case 0x100: case 0x200: case 0x300:
+				case 0x400: case 0x500: case 0x600: case 0x700:
+				case 0x800: case 0x900: case 0xA00: case 0xB00:
+				case 0xC00: case 0xD00:
+					return mmu->wram[address & 0x1FFF];
+					
+				// Graphics: object attribute memory
+				// OAM is 160 bytes, remaining bytes read as 0
+				case 0xE00:
+					if(address < 0xFEA0)
+						return GPU->oam[address & 0xFF];
+					else
+						return 0;
+				
+				// Zero-page
+			case 0xF00:
+				if(address >= 0xFF80)
+				{
+					return mmu->zram[address & 0x7F];
+				}
+				else
+				{
+					// I/O control handling
+					// Currently unhandled
+					return 0;
+				}
+		}
+	}
+}
+
+word readWord(MMU* mmu, addr address) {
+	return (word)readByte(mmu, address) & ((word)readByte(mmu, address+1)<<8);
+}
+
+void writeByte(MMU* mmu, addr address, byte data); // Write 8-bit byte to a given address
+void writeWord(MMU* mmu, addr address, word data); // Write 16-bit word to a given address
--- MMU.h
+++ MMU.h
@@ -7,13 +7,26 @@
 //
 
 #include "DataTypes.h"
+#include <stdbool.h>
 
 typedef struct {
 	
+	//byte bios[0xFFFF];
+	byte rom0[0x4000];
+	byte rom1[0x4000];
+	byte vram[0x2000];
+	byte eram[0x2000];
+	byte wram[0x2000];
+	byte zram[0xFFFF];
+	
+	bool inBIOS;
+
 } MMU;
 
-void readByte(MMU* mmu, addr address); // Read 8-bit byte from a given address
-void readWord(MMU* mmu, addr address); // Read 16-bit word from a given address
+void initMMU(MMU* mmu);
+
+byte readByte(MMU* mmu, addr address); // Read 8-bit byte from a given address
+word readWord(MMU* mmu, addr address); // Read 16-bit word from a given address
 
 void writeByte(MMU* mmu, addr address, byte data); // Write 8-bit byte to a given address
 void writeWord(MMU* mmu, addr address, word data); // Write 16-bit word to a given address
